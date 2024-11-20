bfec69198afd3e1e2b8a6e1af54b41ddfe5a1c16
Mark Armstrong <markparmstrong@gmail.com>
2024-11-20 02:49:44 -0500

    feat: add solution for day 2 part 1 [aoc2022]

--- advent-of-code-2022/day02/part1.go
+++ advent-of-code-2022/day02/part1.go
@@ -1,10 +1,43 @@
 package day02
 
 import (
+	"strings"
+
 	"github.com/armkeh/coding-challenges/advent-of-code-2022/settings"
-	// "github.com/armkeh/coding-challenges/advent-of-code-2022/utils"
+	"github.com/armkeh/coding-challenges/advent-of-code-2022/utils"
+	"fmt"
 )
 
-func Part1(_ settings.Config) (string, error) {
-	return "Not yet implemented", nil
+func Part1(c settings.Config) (string, error) {
+	input, err := utils.GetInputByLines(c.InputPath)
+	if err != nil {
+		return "", err
+	}
+
+	score := 0
+	for line, s := range input {
+		// Skip empty lines
+		if s == "" {
+			continue
+		}
+
+		plays := strings.Split(s, " ")
+		if len(plays) != 2 {
+			return "", fmt.Errorf("Encountered input %s on line %d which does not match the play format!", s, line)
+		}
+
+		theirPlay, err := parsePlay(plays[0])
+		if err != nil {
+			return "", err
+		}
+
+		yourPlay, err := parsePlay(plays[1])
+		if err != nil {
+			return "", err
+		}
+
+		score += throw(yourPlay, theirPlay)
+	}
+
+	return fmt.Sprintf("Your total score would be %d", score), nil
 }
