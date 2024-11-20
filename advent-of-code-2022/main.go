4eda5db6d2825a61839910e5f76923151f03c52e
Mark Armstrong <markparmstrong@gmail.com>
2024-11-19 23:19:55 -0500

    chore: improve main input validation [aoc2022]

--- advent-of-code-2022/main.go
+++ advent-of-code-2022/main.go
@@ -25,10 +25,64 @@ func main() {
 		inputPath = fmt.Sprintf("./inputs/day%d.txt", day)
 	}
 
+	unimplemented := func(string)(string, error) {
+		return "", fmt.Errorf("Day %d, part %d not yet implemented!", day, part)
+	}
+
 	var runner func(string)(string, error)
 	switch {
-		case day == 1 && part == 1: runner = day1.Part1
-		default: log.Fatal("Day and part out of range, or not yet implemented!")
+		case day == 1  && part == 1: runner = day1.Part1
+		case day == 1  && part == 2: runner = unimplemented
+		case day == 2  && part == 1: runner = unimplemented
+		case day == 2  && part == 2: runner = unimplemented
+		case day == 3  && part == 1: runner = unimplemented
+		case day == 3  && part == 2: runner = unimplemented
+		case day == 4  && part == 1: runner = unimplemented
+		case day == 4  && part == 2: runner = unimplemented
+		case day == 5  && part == 1: runner = unimplemented
+		case day == 5  && part == 2: runner = unimplemented
+		case day == 6  && part == 1: runner = unimplemented
+		case day == 6  && part == 2: runner = unimplemented
+		case day == 7  && part == 1: runner = unimplemented
+		case day == 7  && part == 2: runner = unimplemented
+		case day == 8  && part == 1: runner = unimplemented
+		case day == 8  && part == 2: runner = unimplemented
+		case day == 9  && part == 1: runner = unimplemented
+		case day == 9  && part == 2: runner = unimplemented
+		case day == 10 && part == 1: runner = unimplemented
+		case day == 10 && part == 2: runner = unimplemented
+		case day == 11 && part == 1: runner = unimplemented
+		case day == 11 && part == 2: runner = unimplemented
+		case day == 12 && part == 1: runner = unimplemented
+		case day == 12 && part == 2: runner = unimplemented
+		case day == 13 && part == 1: runner = unimplemented
+		case day == 13 && part == 2: runner = unimplemented
+		case day == 14 && part == 1: runner = unimplemented
+		case day == 14 && part == 2: runner = unimplemented
+		case day == 15 && part == 1: runner = unimplemented
+		case day == 15 && part == 2: runner = unimplemented
+		case day == 16 && part == 1: runner = unimplemented
+		case day == 16 && part == 2: runner = unimplemented
+		case day == 17 && part == 1: runner = unimplemented
+		case day == 17 && part == 2: runner = unimplemented
+		case day == 18 && part == 1: runner = unimplemented
+		case day == 18 && part == 2: runner = unimplemented
+		case day == 19 && part == 1: runner = unimplemented
+		case day == 19 && part == 2: runner = unimplemented
+		case day == 20 && part == 1: runner = unimplemented
+		case day == 20 && part == 2: runner = unimplemented
+		case day == 21 && part == 1: runner = unimplemented
+		case day == 21 && part == 2: runner = unimplemented
+		case day == 22 && part == 1: runner = unimplemented
+		case day == 22 && part == 2: runner = unimplemented
+		case day == 23 && part == 1: runner = unimplemented
+		case day == 23 && part == 2: runner = unimplemented
+		case day == 24 && part == 1: runner = unimplemented
+		case day == 24 && part == 2: runner = unimplemented
+		case day == 25 && part == 1: runner = unimplemented
+		case day == 25 && part == 2: runner = unimplemented
+
+		default: log.Fatal(fmt.Sprintf("Day (%d) or part (%d) out of range; day must be between 1 and 25, part either 1 or 2!", day, part))
 	}
 
 	result, err := runner(inputPath)
