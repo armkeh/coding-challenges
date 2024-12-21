94aaafeac85e5b946a6e50f24f9511947acb0f95
Mark Armstrong <markparmstrong@gmail.com>
2024-12-21 00:21:11 -0500

    feat: add day 3, part 2 solution [aoc2024]

--- advent-of-code-2024/AdventOfCode2024.java
+++ advent-of-code-2024/AdventOfCode2024.java
@@ -9,6 +9,6 @@ public class AdventOfCode2024 {
     public static void main(String[] args) {
         var inputPath = "inputs/day03.txt";
 
-        System.out.println(Day03.part1(inputPath));
+        System.out.println(Day03.part2(inputPath));
     }
 }
--- advent-of-code-2024/Day03.java
+++ advent-of-code-2024/Day03.java
@@ -7,7 +7,7 @@ public final class Day03 {
     private Day03() {
     }
 
-    static private Pattern mulInstruction = Pattern.compile("mul\\((\\d\\d?\\d?),(\\d\\d?\\d?)\\)");
+    static private Pattern mulInstruction = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
 
     public static String part1(String inputPath) {
         String input;
@@ -30,7 +30,39 @@ public final class Day03 {
         return String.format("The sum of products from valid operations is %d.", sumOfProducts);
     }
 
+    static private Pattern conditionalOrMulInstruction = Pattern.compile("do\\(\\)|don't\\(\\)|mul\\((\\d{1,3}),(\\d{1,3})\\)");
+
     public static String part2(String inputPath) {
-        return "";
+        String input;
+        try {
+            input = Utils.parseAsString(inputPath);
+        } catch (FileNotFoundException e) {
+            return String.format("Input file %s not found; " + e, inputPath);
+        }
+
+        var matcher = conditionalOrMulInstruction.matcher(input);
+
+        var sumOfProducts = 0;
+        var enabled = true;
+        while (matcher.find()) {
+            var match = matcher.group(0);
+            switch (match) {
+            case "do()":
+                enabled = true;
+                break;
+            case "don't()":
+                enabled = false;
+                break;
+            default:
+                if (enabled) {
+                    var termX = Integer.valueOf(matcher.group(1));
+                    var termY = Integer.valueOf(matcher.group(2));
+
+                    sumOfProducts += termX * termY;
+                }
+            }
+        }
+
+        return String.format("The sum of products from valid operations is %d.", sumOfProducts);
     }
 }
