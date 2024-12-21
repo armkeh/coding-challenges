7504e0df88245802f54600cda0b82e8e547ed15f
Mark Armstrong <markparmstrong@gmail.com>
2024-12-20 23:02:45 -0500

    feat: refactor day 2 code to prepare for part 2 (not working yet) [aoc2024]

--- advent-of-code-2024/Day02.java
+++ advent-of-code-2024/Day02.java
@@ -15,37 +15,64 @@ public final class Day02 {
 
         var reports = Utils.parseLinesToIntegerLists(input);
 
-        var safeReports = 0;
-        for (var report : reports) {
-            if (report.size() < 2) {
-                return "Error; not enough levels in a report!";
-            }
-
-            var incrementing = report.get(0) < report.get(1);
-
-            var safetyIncrement = 1;
-            for (var i = 0; i < report.size() - 1; i++) {
-                int diff;
-                if (incrementing) {
-                    diff = report.get(i+1) - report.get(i);
-                } else {
-                    diff = report.get(i) - report.get(i+1);
-                }
-
-                if (diff < 1 || diff > 3) {
-                    // Violation found, stop checking this report and don't increment.
-                    safetyIncrement = 0;
-                    break;
-                }
-            }
-
-            safeReports += safetyIncrement;
-        }
-
-        return String.format("There are %d safe reports.", safeReports);
+        return String.format("There are %d safe reports.", safeReports(reports, 0));
     }
 
-    public static String part2(String inputPath) {
-        return "";
+    private static int safeReports(ArrayList<ArrayList<Integer>> reports, int unsafeLevelToleration) {
+        var safeReports = 0;
+
+        for (var report : reports) {
+            if (isReportSafe(report, unsafeLevelToleration)) {
+                safeReports++;
+            }
+        }
+
+        return safeReports;
+    }
+
+    private static boolean isReportSafe(ArrayList<Integer> report, int unsafeLevelToleration) {
+        if (report.size() < 2) {
+            throw new IllegalArgumentException("Error; not enough levels in a report!");
+        }
+
+        var increasing = report.get(0) < report.get(1);
+        var unsafeLevels = 0;
+        for (var i = 0; i < report.size() - 1; i++) {
+            int diff;
+            if (increasing) {
+                diff = report.get(i+1) - report.get(i);
+            } else {
+                diff = report.get(i) - report.get(i+1);
+            }
+
+            if (diff < 1 || diff > 3) {
+                unsafeLevels++;
+                if (unsafeLevels > unsafeLevelToleration) {
+                    return false;
+                } else {
+                    System.out.println(String.format("Unsafe level found after index %d in report %s!", i, report.toString()));
+
+                    // Try again, with either of levels removed.
+                    var dampenedReport1 = (ArrayList)report.clone();
+                    var dampenedReport2 = (ArrayList)report.clone();
+
+                    dampenedReport1.remove(i);
+                    dampenedReport2.remove(i+1);
+
+                    System.out.println(String.format("Trying again with modified reports %s and %s.\n", dampenedReport1.toString(), dampenedReport2.toString()));
+
+                    var safe = isReportSafe(dampenedReport1, unsafeLevelToleration - 1) || isReportSafe(dampenedReport2, unsafeLevelToleration - 1);
+
+                    if (safe) {
+                        System.out.println("Report was found to be safe after level removed!\n");
+                    }
+
+                    return safe;
+                }
+            }
+        }
+
+        // End of report reached, it is safe.
+        return true;
     }
 }
