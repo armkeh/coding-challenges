9cf63cf2cb2576c5a49556477c0fb0870bb4e8c4
Mark Armstrong <markparmstrong@gmail.com>
2024-12-27 16:02:02 -0500

    feat: add day 5 part 2 solution [aoc2024]

--- advent-of-code-2024/Day05.java
+++ advent-of-code-2024/Day05.java
@@ -57,6 +57,62 @@ public final class Day05 implements Day {
     }
 
     public String part2(String inputPath) {
-        return "Day or part not yet implemented!";
+        String input;
+        try {
+            input = Utils.parseFileToString(inputPath);
+        } catch (FileNotFoundException e) {
+            return String.format("Input file %s not found; " + e, inputPath);
+        }
+
+        var orderingRuleMatcher = orderingRule.matcher(input);
+
+        var orderingRules = new ArrayList<Tuple<String,String>>();
+        while (orderingRuleMatcher.find()) {
+            var beforePage = orderingRuleMatcher.group(1);
+            var afterPage  = orderingRuleMatcher.group(2);
+
+            orderingRules.add(new Tuple<String,String>(beforePage, afterPage));
+        }
+
+        var pageListMatcher = pageList.matcher(input);
+
+        var middlePageNumbersSum = 0;
+        while (pageListMatcher.find()) {
+            var pageList = pageListMatcher.group();
+            var pages = Arrays.asList(pageList.split(","));
+
+            var obeysRules = true;
+            var scanNeeded = true;
+            while (swapPerformed) {
+                // By default, don't rescan.
+                scanNeeded = false;
+
+                for (var rule : orderingRules) {
+                    var beforePage  = rule.x;
+                    var beforeIndex = pages.indexOf(beforePage);
+                    var afterPage   = rule.y;
+                    var afterIndex  = pages.indexOf(afterPage);
+
+                    if (beforeIndex != -1 && afterIndex != -1 && beforeIndex > afterIndex) {
+                        obeysRules = false;
+
+                        // Correct the broken rule by swapping the pages.
+                        pages.set(beforeIndex, afterPage);
+                        pages.set(afterIndex, beforePage);
+
+                        // Re-scan after the swap(s).
+                        scanNeeded = true;
+                    }
+                }
+            }
+
+            if (!obeysRules) {
+                var middlePage = Integer.valueOf(pages.get(pages.size() / 2));
+
+                middlePageNumbersSum += middlePage;
+            }
+        }
+
+        return String.format("The sum of middle page numbers from corrected updates is %d.", middlePageNumbersSum);
     }
 }
