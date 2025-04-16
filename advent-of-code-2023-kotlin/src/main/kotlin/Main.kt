47f175749c528a01e85796adc6e49d5f462d7fa3
Mark Armstrong <markparmstrong@gmail.com>
2025-04-15 23:55:41 -0400

    feat: add inputs directory, add solution for part 2 of day 1 [aoc2023]

--- advent-of-code-2023-kotlin/src/main/kotlin/Main.kt
+++ advent-of-code-2023-kotlin/src/main/kotlin/Main.kt
@@ -1,14 +1,11 @@
 package io.github.armkeh
 
-val input = """
-        1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet
-        """.trimIndent()
+import java.io.File
 
-fun main() {
-    val sum = input.lines().sumOf { line ->
+val day01Input = File("src/inputs/day01.txt")
+
+fun day01Part01(input: List<String>) {
+    val sum = input.sumOf { line ->
         val tens = line.first { it.isDigit() }.toString().toInt()
         val ones = line.last  { it.isDigit() }.toString().toInt()
 
@@ -16,4 +13,31 @@ fun main() {
     }
 
     println("Sum of calibration values is $sum")
+}
+
+fun day01Part02(input: List<String>) {
+    // Because some letters may be shared between two adjacent digit words (e.g., eightwo),
+    // to solve this using replacement, we must preserve those letters
+    // while also inserting the numerals for the words.
+    // The simplest approach is to place two copies of the word, around the numeral.
+    // We could be more clever and determine which letters are strictly needed, but no need to over-optimize.
+    val revisedInput = input.map { it
+        .replace("one",   "one1one", ignoreCase = true)
+        .replace("two",   "two2two", ignoreCase = true)
+        .replace("three", "three3three", ignoreCase = true)
+        .replace("four",  "four4four", ignoreCase = true)
+        .replace("five",  "five5five", ignoreCase = true)
+        .replace("six",   "six6six", ignoreCase = true)
+        .replace("seven", "seven7seven", ignoreCase = true)
+        .replace("eight", "eight8eight", ignoreCase = true)
+        .replace("nine",  "nine9nine", ignoreCase = true)
+        // "zero" was not identified as a valid digit word.
+        // .replace("zero",  "0", ignoreCase = true)
+    }
+
+    day01Part01(revisedInput)
+}
+
+fun main() {
+    day01Part02(day01Input.readLines())
 }
